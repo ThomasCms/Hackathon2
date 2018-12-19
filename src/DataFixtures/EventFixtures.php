0588abf9993d02d0b32ea7716cae245e31d8df97
Thomas Pecout <pecoutt@hotmail.fr>
2018-12-19 19:18:33 +0100

    fixture event

---
+++ src/DataFixtures/EventFixtures.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder2
+ * Date: 19/12/18
+ * Time: 18:47
+ */
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Event;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class EventFixtures extends Fixture
+{
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies()
+    {
+        // TODO: Implement getDependencies() method.
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $toto = Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 10; $i++) {
+
+            $part = new Event();
+            $part->setNom(ucwords($toto->sentence));
+            $part->setDate($toto->dateTimeBetween('-1 month', '+1 years'));
+            $part->setLieu($toto->address);
+            $part->setDescription($toto->text);
+            $part->setBilan($toto->text);
+            $manager->persist($part);
+
+        }
+        $manager->flush();
+    }
+}
