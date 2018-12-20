46ea0cf414d61ee5234f9d3fac841bb72830d00a
Thomas Pecout <pecoutt@hotmail.fr>
2018-12-20 10:54:18 +0100

    toto

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,21 @@ class EventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('date')
-            ->add('lieu')
-            ->add('description')
-            ->add('bilan')
+            ->add('nom', TextType::class, [
+                'attr' => ['class' => 'form']
+            ])
+            ->add('date', DateType::class, [
+                'attr' => ['class' => 'datepicker']
+            ])
+            ->add('lieu', TextType::class, [
+                    'attr' => ['class' => 'form']
+                ])
+            ->add('description', TextareaType::class, [
+                'attr' => ['class' => 'form']
+            ])
+            ->add('bilan', TextareaType::class, [
+                'attr' => ['class' => 'form']
+            ])
         ;
     }
 
