1b4391f1b9ba8f7f581ce5ba11fa4167bdbb8376
HOARAU Mathieu <Mathieuhoarau@laposte.net>
2018-12-20 17:56:58 +0100

    mm

--- src/DataFixtures/RetourEventFixtures.php
+++ src/DataFixtures/RetourEventFixtures.php
@@ -13,19 +13,19 @@ class RetourEventFixtures extends Fixture
     {
         $toto = Faker\Factory::create('fr_FR');
         // create 1000 events! Bam!
-        for ($i = 0; $i < 1000; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             $product = new RetourEvent();
             $product->setIdEvenement($i);
             $product->setCategorieEvenement($toto->text);
             $product->setDateEvenement($toto->dateTimeBetween('-1 month', '+1 years'));
             $product->setExplicationEvenement($toto->text);
-            $product->setNbreInscritsEvenement($toto->numberBetween(10, 100));
-            $product->setPresentsEvenement($toto->numberBetween(10, 100));
-            $product->setNbreStartupEvenement($toto->numberBetween(10, 100));
-            $product->setNbrePartenaireEvenement($toto->numberBetween(10, 100));
-            $product->setNbreExternesEvenement($toto->numberBetween(10, 100));
-            $product->setNbrePmeEvenement($toto->numberBetween(10, 100));
-            $product->setNbrePorteurProjetEvenement($toto->numberBetween(10, 100));
+            $product->setNbreInscritsEvenement($toto->numberBetween(50, 100));
+            $product->setNbreStartupEvenement($toto->numberBetween(1, 10));
+            $product->setNbrePartenaireEvenement($toto->numberBetween(1, 10));
+            $product->setNbreExternesEvenement($toto->numberBetween(1, 10));
+            $product->setNbrePmeEvenement($toto->numberBetween(1, 10));
+            $product->setNbrePorteurProjetEvenement($toto->numberBetween(1, 10));
+            $product->setPresentsEvenement($product->getNbreExternesEvenement()+$product->getNbrePartenaireEvenement()+$product->getNbrePmeEvenement()+$product->getNbrePorteurProjetEvenement()+$product->getNbreStartupEvenement());
             $product->setResultatEvenement($toto->text);
             $product->setLiensFormulairesEvenement($toto->text);
             $product->setOrigineParticipationEvenement($toto->text);
