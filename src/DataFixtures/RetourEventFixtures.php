07b50b9a5a28daf21f9eb134307f61fea1765d82
HOARAU Mathieu <Mathieuhoarau@laposte.net>
2018-12-21 10:28:53 +0100

    m

--- src/DataFixtures/RetourEventFixtures.php
+++ src/DataFixtures/RetourEventFixtures.php
@@ -16,9 +16,9 @@ class RetourEventFixtures extends Fixture
         for ($i = 0; $i < 10; $i++) {
             $product = new RetourEvent();
             $product->setIdEvenement($i);
-            $product->setCategorieEvenement($toto->text);
+            $product->setCategorieEvenement($toto->text(10));
             $product->setDateEvenement($toto->dateTimeBetween('-1 month', '+1 years'));
-            $product->setExplicationEvenement($toto->text);
+            $product->setExplicationEvenement($toto->text(10));
             $product->setNbreInscritsEvenement($toto->numberBetween(50, 100));
             $product->setNbreStartupEvenement($toto->numberBetween(1, 10));
             $product->setNbrePartenaireEvenement($toto->numberBetween(1, 10));
@@ -26,12 +26,12 @@ class RetourEventFixtures extends Fixture
             $product->setNbrePmeEvenement($toto->numberBetween(1, 10));
             $product->setNbrePorteurProjetEvenement($toto->numberBetween(1, 10));
             $product->setPresentsEvenement($product->getNbreExternesEvenement()+$product->getNbrePartenaireEvenement()+$product->getNbrePmeEvenement()+$product->getNbrePorteurProjetEvenement()+$product->getNbreStartupEvenement());
-            $product->setResultatEvenement($toto->text);
-            $product->setLiensFormulairesEvenement($toto->text);
-            $product->setOrigineParticipationEvenement($toto->text);
+            $product->setResultatEvenement($toto->text(10));
+            $product->setLiensFormulairesEvenement($toto->text(10));
+            $product->setOrigineParticipationEvenement($toto->text(10));
             $product->setNbreMailInvitationEvenement($toto->numberBetween(10, 100));
             $product->setTauxSatisfactionEvenement($toto->numberBetween(10, 100));
-            $product->setSuggestionEvenement($toto->text);
+            $product->setSuggestionEvenement($toto->text(10));
 
             $manager->persist($product);
         }
